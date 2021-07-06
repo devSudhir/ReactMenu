41556419737d3cb1d2c7aa05eacbe5790b304df1
Sudhir <82999615+devSudhir@users.noreply.github.com>
2021-07-06 21:40:35 +0530

    Add files via upload
    src folder

---
+++ src/components/Download.jsx
@@ -0,0 +1,4 @@
+import {aBackColorBrown } from "./utility.module.css"
+export function Download() {
+    return <a href="/" className={ aBackColorBrown}>DOWNLOAD</a>
+}
---
+++ src/components/Settings.jsx
@@ -0,0 +1,5 @@
+import {aBackColorLightGray } from "./utility.module.css"
+
+export function Settings() {
+    return <a href="/" className={ aBackColorLightGray}>SETTINGS</a>
+}
